2cc77ca412146810c174205101eebc7d239de55e
Kyrilios <Cyriljohnamoronio@outlook.com>
2020-02-23 19:14:47 +0800

    added music

--- js/scenes/Scene1.js
+++ js/scenes/Scene1.js
@@ -1,27 +1,43 @@
 class Scene1 extends Phaser.Scene {
   constructor() {
     super("bootGame");
+    
   }
+
+  
 preload(){
 
   this.load.image('play', 'assets/images/play.png');
   this.load.image('BG', 'assets/images/BG.png');
-  // game.load.audio('torikago', ['assets/Audio/torikago.mp3']);
+  this.load.audio('theme', [
+    'assets/Audio/Torikago.ogg'
+])
+  
+  
 
 
 }
 
   create() {
-   // this.stage.backgroundColor = "#4488AA";
+    this.Em = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
+    this.beamSound = this.sound.add("theme");
     this.add.image(500,400 ,"BG");
-    // music = game.add.audio('torikago');
-    // music.play();
+   
+    
     this.titleText = this.add.text(520, 100, 'mArIo GaMe', {fontSize:"60px", fill: '#00ffff' });
     const helloButton = this.add.image(700, 480, 'play').setScale(.5); 
     helloButton.setInteractive();
     helloButton.on('pointerdown', () => { 
     this.scene.start("playGame");
     });
+  }
+  update(){
+
+    if (Phaser.Input.Keyboard.JustDown(this.Em)) {
+      
+      this.beamSound.play();
+      }
+
   }
 }
 
